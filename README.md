d0ef3b5ab3ee110678f36021c2c0eade66628e9b
Hector Moreno Veleiro <hmorenoveleiro@gmail.com>
2026-09-16 13:52:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Inicializar el proyecto ##
+```
+npm init
+```
+## Descargar una dependencia (vite) ##
+```
+npm i -D vite
+```
+## Descargar una dependencia de producción ##
+```
+npm run dayjs
+```
+## Probar la app en modo local (desarrollo) ##
+```
+npm run dev
+```
+## Construir la app ##
+```
+npm run build
+```
+
+## Arrancar la aplicación ##
+```
+npm run preview
+```
---
+++ src/main.js
@@ -0,0 +1,48 @@
+// src/main.js
+// Lenguaje no tipado
+
+import dayjs from "dayjs";// importamos la librería dayjs para trabajar con fechas
+
+// creamos boton para guardar la acción
+const boton = document.getElementById("Add");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+
+// le añadimos un listener al boton para ejecute la función saveAction
+boton.addEventListener("click", saveAction);
+
+// función que se ejecuta al hacer click en el boton
+function saveAction() {
+    const fechaHoy = dayjs().format("HH:mm"); // fecha de hoy formato DD/MM/YYYY
+    const li = document.createElement("li");// creamos un elemento li
+    li.textContent = fechaHoy + ": " + input.value + " "; // valor del input y la fecha en el li
+    list.appendChild(li); // añadimos el li a la lista
+    input.value = ""; // limpiamos el input
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
