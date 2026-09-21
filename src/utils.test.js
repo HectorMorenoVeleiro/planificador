57a4b2932f002bc84d711cf2d08b373d8915aa42
HectorMorenoVeleiro <hmorenoveleiro@gmail.com>
2026-09-21 14:43:00 +0200

    .

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,7 +2,6 @@ import { formatearFecha } from "./utils"
 import { describe, it, expect } from "vitest"
 
 describe('formatear fecha', () => {
-
     it('formatea correctamente una fecha simple', () => {
         const fecha = new Date(2026, 8, 21, 14, 33)
         const resultado = formatearFecha(fecha)
