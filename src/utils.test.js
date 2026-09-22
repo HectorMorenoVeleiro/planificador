60b08aefed9c9a4b5b1d36d4b0de1bdd0a0535b8
HectorMorenoVeleiro <hmorenoveleiro@gmail.com>
2026-09-22 14:23:41 +0200

    .

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,11 +1,19 @@
-import { formatearFecha } from "./utils"
+import { calcularPeso, formatearFecha } from "./utils"
 import { describe, it, expect } from "vitest"
 
 describe('formatear fecha', () => {
     it('formatea correctamente una fecha simple', () => {
         const fecha = new Date(2026, 8, 21, 14, 33)
         const resultado = formatearFecha(fecha)
-        expect(resultado).toBe("21/09/2026 14:33")
+        expect(resultado).toBe('21/09/2026 14:33')
     })
 })
-// solucionar toda esta mierda
+
+describe('calcular peso', () => {
+    it('calcular peso a partir de unos minutos dados', () => {
+        const minutos = 35;
+        const peso = calcularPeso(minutos);
+        expect(peso).toBe('alto');
+    })
+    
+})
