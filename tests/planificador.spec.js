4a4712b58c43d4dbc7e5bbcf48409ba7d93d845d
HectorMorenoVeleiro <hmorenoveleiro@gmail.com>
2026-09-23 14:30:02 +0200

    .

--- tests/planificador.spec.js
+++ tests/planificador.spec.js
@@ -6,3 +6,13 @@ test('Muestra el titulo de la app' , async ({page}) => {
     await expect(page.getByRole('heading', {name: 'Course Planning'})).toBeVisible();
 
 }) // primer test sencillito creado
+
+test('Vamos a añadir una nueva actividad', async({page})=> {
+    await page.goto('/')
+    await page.getByPlaceholder('Escribe lo que has hecho hoy').fill('Cantar')
+    await page.getByPlaceholder('60 min').fill('5')
+    await page.getByRole('button', {name: 'Añadir'}).click()
+    await expect(
+        page.getByText('Cantar')
+    )
+})
